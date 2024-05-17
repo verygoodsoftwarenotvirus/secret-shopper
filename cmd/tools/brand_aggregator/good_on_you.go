dc254a921a36123a5dd0401b7a61c1121d666331
Jeffrey <verygoodsoftwarenotvirus@protonmail.com>
2024-05-17 07:52:14 -0500

    first stab at good on you

---
+++ cmd/tools/brand_aggregator/good_on_you.go
@@ -0,0 +1,145 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+)
+
+type GoodOnYouPayload struct {
+	Category []string         `json:"category"`
+	Offset   int              `json:"offset"`
+	Filters  GoodOnYouFilters `json:"filters"`
+}
+
+type GoodOnYouFilters struct {
+	Category []string `json:"category"`
+}
+
+func fetchGoodOnYouResult() {
+	data := Payload{
+		// fill struct
+	}
+	payloadBytes, err := json.Marshal(data)
+	if err != nil {
+		// handle err
+	}
+	body := bytes.NewReader(payloadBytes)
+
+	req, err := http.NewRequest("POST", "https://public-api.goodonyou.eco/parse/functions/browseCategoriesV4", body)
+	if err != nil {
+		// handle err
+	}
+	req.Header.Set("Content-Type", "application/json;charset=utf-8")
+	req.Header.Set("X-Parse-Application-Id", "gcrp2V42PHW7S8ElL639")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		// handle err
+	}
+	defer resp.Body.Close()
+}
+
+type GoodOnYouQueryResult struct {
+	Result struct {
+		Page     int `json:"page"`
+		Total    int `json:"total"`
+		Category struct {
+			ID     string   `json:"id"`
+			Name   string   `json:"name"`
+			Slug   string   `json:"slug"`
+			Image  string   `json:"image"`
+			Gender []string `json:"gender"`
+			Qty    int      `json:"qty"`
+			Total  struct {
+				All   int `json:"all"`
+				Kids  int `json:"kids"`
+				Men   int `json:"men"`
+				Women int `json:"women"`
+			} `json:"total"`
+			Subcategories []string `json:"subcategories"`
+		} `json:"category"`
+		Categories []struct {
+			ID     string   `json:"id"`
+			Name   string   `json:"name"`
+			Slug   string   `json:"slug"`
+			Image  string   `json:"image"`
+			Gender []string `json:"gender"`
+			Qty    int      `json:"qty"`
+			Total  struct {
+				All   int `json:"all"`
+				Kids  int `json:"kids"`
+				Men   int `json:"men"`
+				Women int `json:"women"`
+			} `json:"total"`
+			Subcategories []string `json:"subcategories"`
+		} `json:"categories"`
+		Filters struct {
+			Territories []struct {
+				Code string `json:"code"`
+				Name string `json:"name"`
+			} `json:"territories"`
+			Locations struct {
+				Eu GoodOnYouRegion `json:"EU"`
+				Na GoodOnYouRegion `json:"NA"`
+				Cs GoodOnYouRegion `json:"CS"`
+				Oc GoodOnYouRegion `json:"OC"`
+				Af GoodOnYouRegion `json:"AF"`
+				As GoodOnYouRegion `json:"AS"`
+			} `json:"locations"`
+			Values        []GoodOnYouCompanyValues `json:"values"`
+			Subcategories []GoodOnYouSubcategory   `json:"subcategories"`
+		} `json:"filters"`
+		Picks  []GoodOnYouPick  `json:"picks"`
+		Brands []GoodOnYouBrand `json:"brands"`
+	} `json:"result"`
+}
+
+type GoodOnYouRegion struct {
+	Code      string             `json:"code"`
+	Name      string             `json:"name"`
+	Countries []GoodOnYouCountry `json:"countries"`
+}
+
+type GoodOnYouCountry struct {
+	Code      string `json:"code"`
+	Name      string `json:"name"`
+	Continent string `json:"continent"`
+}
+
+type GoodOnYouCompanyValues struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+type GoodOnYouSubcategory struct {
+	ID     string   `json:"id"`
+	Name   string   `json:"name"`
+	Gender []string `json:"gender"`
+}
+
+type GoodOnYouPick struct {
+	ID            string `json:"id"`
+	Name          string `json:"name"`
+	Slug          string `json:"slug"`
+	Image         string `json:"image"`
+	EthicalLabel  string `json:"ethical_label"`
+	EthicalRating int    `json:"ethical_rating"`
+	Price         int    `json:"price"`
+	Order         int    `json:"order"`
+}
+
+type GoodOnYouBrand struct {
+	ID            string   `json:"id"`
+	Name          string   `json:"name"`
+	Slug          string   `json:"slug"`
+	Image         string   `json:"image"`
+	EthicalLabel  string   `json:"ethical_label"`
+	EthicalRating int      `json:"ethical_rating"`
+	Price         int      `json:"price"`
+	Territory     []string `json:"territory"`
+	Women         int      `json:"women"`
+	Men           int      `json:"men"`
+	Kids          int      `json:"kids"`
+	IsStaffPick   bool     `json:"isStaffPick,omitempty"`
+}
